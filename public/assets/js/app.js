97cdd0d5c86d74166aa003969f2190f67e608673
Valerie Riley <valeriesriley@gmail.com>
2020-02-09 11:16:15 -0700

    created format for basic HTML and API routes

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -11,6 +11,31 @@ app.use(express.static("assets"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// return index.html
+app.get("/", function (req, res) {
+
+});
+
+// return notes.html
+app.get("/notes", function (req, res) {
+
+});
+
+// read db.json file and return all saved notes as JSOn
+app.get("/api/notes", function (req,res) {
+
+});
+
+// receive new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+app.post("/api/notes", function (req,res) {
+
+});
+
+// remove note with given id
+app.delete("/api/notes/:id", function (req,res) {
+
+});
+
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
